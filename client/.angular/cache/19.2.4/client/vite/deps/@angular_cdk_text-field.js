1a0a6e8b1a667c96c3239bedec2f6775eba5f86c
Vika <siktorija.viktorija@gmail.com>
2025-03-24 15:57:08 +0100

    Removed .angular/cache from Git tracking and added to gitignore

---
+++ client/.angular/cache/19.2.4/client/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,15 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-K4TJVFXS.js";
+import "./chunk-7EZ74L25.js";
+import "./chunk-EXOEQGAB.js";
+import "./chunk-PFLGQ7LY.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
